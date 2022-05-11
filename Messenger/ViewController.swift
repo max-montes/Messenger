e91b922c69951c2daad7f8cca03b4ad0bbd4fb10
max-montes <77820353+max-montes@users.noreply.github.com>
2022-05-11 16:51:29 -0400

    Initial Commit

---
+++ Messenger/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Messenger
+//
+//  Created by Max Montes on 5/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
